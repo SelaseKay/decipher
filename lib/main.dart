c13069de6683e2572c2a7846b038bad5cf9a7158
SelaseKay <judekwashie70@gmail.com>
2023-08-04 11:16:39 +0000

    remove unused variable

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late final Future<SharedPreferences> _prefs;
-  late final bool? _hasOnboarded;
 
   @override
   void initState() {
